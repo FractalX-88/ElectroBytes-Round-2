4724056501ec8e92e8c29ee8fb2f61555ca7f7d5
FractalX-88 <darshansamuelx@gmail.com>
2026-03-26 15:50:54 +0530

    Update public_check.py

--- public_check.py
+++ public_check.py
@@ -2,18 +2,18 @@ import os
 import subprocess
 import platform
 
-QUESTIONS = ["q1", "q2", "q3", "q4", "q5"]
+QUESTIONS = ["Q1_LogicGates", "Q2_WPattern", "Q3_StudentSearch", "Q4_RoundRobin", "Q5_TowerOFHanoi"]
 IS_WINDOWS = platform.system() == "Windows"
 PYTHON_CMD = "python" if IS_WINDOWS else "python3"
 
 # Only the visible Sample Test Cases
 TESTS = {
-    "q1": [("0 0", "BUFFER: 0\nNOT: 1\nAND: 0\nOR: 0\nNAND: 1\nNOR: 1\nXOR: 0\nXNOR: 1"), 
+    "Q1_LogicGates": [("0 0", "BUFFER: 0\nNOT: 1\nAND: 0\nOR: 0\nNAND: 1\nNOR: 1\nXOR: 0\nXNOR: 1"), 
            ("1 0", "BUFFER: 1\nNOT: 0\nAND: 0\nOR: 1\nNAND: 1\nNOR: 0\nXOR: 1\nXNOR: 0")],
-    "q2": [("2", "* * *\n * *"), ("3", "* * *\n * * * *\n  * *")],
-    "q3": [("5\n5 4 3 2 1\n3", "1 2 3 4 5\n2"), ("4\n10 20 30 40\n50", "10 20 30 40\n-1")],
-    "q4": [("3\n10 5 8\n2", "61"), ("2\n4 4\n2", "12")],
-    "q5": [("3", "Total Moves: 7\nFirst move of Disk 1: 1"), ("4", "Total Moves: 15\nFirst move of Disk 1: 1")]
+    "Q2_WPattern": [("2", "* * *\n * *"), ("3", "* * *\n * * * *\n  * *")],
+    "Q3_StudentSearch": [("5\n5 4 3 2 1\n3", "1 2 3 4 5\n2"), ("4\n10 20 30 40\n50", "10 20 30 40\n-1")],
+    "Q4_RoundRobin": [("3\n10 5 8\n2", "61"), ("2\n4 4\n2", "12")],
+    "Q5_TowerOFHanoi": [("3", "Total Moves: 7\nFirst move of Disk 1: 1"), ("4", "Total Moves: 15\nFirst move of Disk 1: 1")]
 }
 
 def run_test(cmd, stdin_data):
